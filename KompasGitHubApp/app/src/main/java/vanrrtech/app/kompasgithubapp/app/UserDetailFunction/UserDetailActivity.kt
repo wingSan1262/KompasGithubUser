c2ef039d6bafa44513da8fbd6c8a4f982d28a2e6
Wing Wira <wira1262@gmail.com>
2022-01-17 05:26:39 +0700

    add file for user detail module UI

---
+++ KompasGitHubApp/app/src/main/java/vanrrtech/app/kompasgithubapp/app/UserDetailFunction/UserDetailActivity.kt
@@ -0,0 +1,12 @@
+package vanrrtech.app.kompasgithubapp.app.UserDetailFunction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vanrrtech.app.kompasgithubapp.R
+
+class UserDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
