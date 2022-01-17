42f5cf036b69d172c5bc292529025e1427390b61
Wing Wira <wira1262@gmail.com>
2022-01-17 21:42:18 +0700

    Unit Test

---
+++ KompasGitHubApp/app/src/test/java/vanrrtech/app/kompasgithubapp/app/SearchFunction/View/MainActivityTest.kt
@@ -0,0 +1,16 @@
+package vanrrtech.app.kompasgithubapp.app.SearchFunction.View
+
+import junit.framework.TestCase
+
+class MainActivityTest : TestCase() {
+
+    fun testGetViewModel() {}
+
+    fun testSetViewModel() {}
+
+    fun testGetMCompositeDisposable() {}
+
+    fun testOnCreate() {}
+
+    fun testOnDestroy() {}
+}
---
+++ KompasGitHubApp/app/src/test/java/vanrrtech/app/kompasgithubapp/app/UserDetailFunction/ModelView/ViewModelUserDetailsActivityTest.kt
@@ -0,0 +1,10 @@
+package vanrrtech.app.kompasgithubapp.app.UserDetailFunction.ModelView
+
+import junit.framework.TestCase
+
+class ViewModelUserDetailsActivityTest : TestCase() {
+
+    fun testGetUserRepo() {}
+
+    fun testSetUserRepo() {}
+}
