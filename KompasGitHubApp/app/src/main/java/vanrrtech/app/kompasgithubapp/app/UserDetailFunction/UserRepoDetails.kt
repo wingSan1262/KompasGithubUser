5088ab81f72fdceb286aadf6cd72d66f7f90863c
Wing Wira <wira1262@gmail.com>
2022-01-17 20:24:51 +0700

    add manifest and build gradle setting (dependancy etc)

--- KompasGitHubApp/app/src/main/java/vanrrtech/app/kompasgithubapp/app/UserDetailFunction/UserRepoDetails.kt
+++
@@ -1,8 +0,0 @@
-package vanrrtech.app.kompasgithubapp.app.UserDetailFunction
-
-data class UserRepoDetails(
-    var name : String,
-    var description : String,
-    var watcher_count : Int,
-    var update_at : String
-)
